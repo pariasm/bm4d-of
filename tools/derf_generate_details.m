e60257016b2d85333f2528b583493e35eae71c30
parias <pariasm@gmail.com>
2015-08-19 09:47:03 +0200

    [tools] Improvements to ease creation of figures

--- tools/derf_generate_details.m
+++ tools/derf_generate_details.m
@@ -6,54 +6,124 @@ base_path = '/media/pariasm/tera/funes/denoising/';
 bm4d_path = [base_path 'algos/results/VBM4D/'];
 orig_path = [base_path 'data/derf/'];
 %nlb3_path = [base_path 'projects/video_nlbayes3d/results/vnlbayes/table_1ps5_2ps5_2wx37_2r16_2np160/'];
-nlb3_path = [base_path 'projects/video_nlbayes3d/results/vnlbayes/table_1ps7_2ps5_2wx45_r16/'];
+%nlb3_path = [base_path 'projects/video_nlbayes3d/results/vnlbayes/table_1ps7_2ps5_2wx45_r16/'];
+nlb3_colo_path = [base_path 'projects/video_nlbayes3d/results/vnlbayes/table_1ps5_2ps5_tip/'];
+nlb3_mono_path = [base_path 'projects/video_nlbayes3d/results/vnlbayes/table_1ps7_2ps7_tip/'];
+nisy_path = bm4d_path;
 
 
 % generated tiles ------------------- %
-% tennis       050-090-030 150+150+30 %
-% tennis       040-150-070 150+150+30 %
-% bus          060-020-020 150+150+30 %
-% bus          110-150-010 150+150+30 %
-% stefan_mono  090-180-265 150+150+30 %
-% stefan_mono  060-130-005 150+150+30 %
-% mobile_mono  060-060-080 100+100+30 %
-% mobile_mono  188-188-065 100+100+30 %
+% tennis       050,090,030 150,150,30 %
+% tennis       040,150,070 150,150,30 %
+% bus          060,020,020 150,150,30 %
+% bus          110,150,010 150,150,30 %
+% stefan_mono  090,180,265 150,150,30 %
+% stefan_mono  060,130,005 150,150,30 %
+% mobile_mono  060,060,080 100,100,30 %
+% mobile_mono  188,188,065 100,100,30 %
 % ----------------------------------- %
 
-ti = 060;
-tj = 060;
-tt = 080;
-tile_i = [ti:ti + 100];
-tile_j = [tj:tj + 100];
-tile_t = [tt:tt +  30];
-
-addpath '../../../../time_filter/src/';
-
 % sequence data
-seqnames = {'football_mono','mobile_mono','stefan_mono','tennis_mono'};
-first    = {1              , 1           , 1           , 1           };
-last     = {260            , 300         , 300         , 150         };
-%seqnames = {'bus','coastguard','foreman','tennis'};
-%first    = {1    , 1          , 1       , 1      };
-%last     = {150  , 300        , 300     , 150    };
+sequences = struct([]);
 
-sequences = struct('name',seqnames,'first',first,'last',last);
-clear seqnames first last
+sequences(1).name = 'football_mono';
+sequences(1).first = 1;
+sequences(1).last = 260;
+sequences(1).channels = 1;
+
+sequences(2).name = 'mobile_mono';
+sequences(2).first = 1;
+sequences(2).last = 300;
+sequences(2).channels = 1;
+
+sequences(3).name = 'stefan_mono';
+sequences(3).first = 1;
+sequences(3).last = 300;
+sequences(3).channels = 1;
+
+sequences(4).name = 'tennis_mono';
+sequences(4).first = 1;
+sequences(4).last = 150;
+sequences(4).channels = 1;
+
+sequences(5).name = 'bus';
+sequences(5).first = 1;
+sequences(5).last = 150;
+sequences(5).channels = 3;
+
+sequences(6).name = 'coastguard';
+sequences(6).first = 1;
+sequences(6).last = 300;
+sequences(6).channels = 3;
+
+sequences(7).name = 'foreman';
+sequences(7).first = 1;
+sequences(7).last = 300;
+sequences(7).channels = 3;
+
+sequences(8).name = 'tennis';
+sequences(8).first = 1;
+sequences(8).last = 150;
+sequences(8).channels = 3;
+
+
+tiles = struct([]);
+
+tiles(1).seq = sequences(8);
+tiles(1).tile_b = [050,090,030];
+tiles(1).tile_e = tiles(1).tile_b + [150,150, 30];
+
+tiles(2).seq = sequences(8);
+tiles(2).tile_b = [040,150,070];
+tiles(2).tile_e = tiles(2).tile_b + [150,150, 30];
+
+tiles(3).seq = sequences(5);
+tiles(3).tile_b = [060,020,020];
+tiles(3).tile_e = tiles(3).tile_b + [150,150, 30];
+
+tiles(4).seq = sequences(5);
+tiles(4).tile_b = [110,150,010];
+tiles(4).tile_e = tiles(4).tile_b + [150,150, 30];
+
+tiles(5).seq = sequences(3);
+tiles(5).tile_b = [090,180,265];
+tiles(5).tile_e = tiles(5).tile_b + [150,150, 30];
+
+tiles(6).seq = sequences(3);
+tiles(6).tile_b = [060,130,005];
+tiles(6).tile_e = tiles(6).tile_b + [150,150, 30];
+
+tiles(7).seq = sequences(2);
+tiles(7).tile_b = [060,060,080];
+tiles(7).tile_e = tiles(7).tile_b + [100,100, 30];
+
+tiles(8).seq = sequences(2);
+tiles(8).tile_b = [188,188,065];
+tiles(8).tile_e = tiles(8).tile_b + [100,100, 30];
+
+
+%addpath '../../time_filter/src/';
+
 
 sigmas = {'10', '20', '40'};
 
 %for vnlb = ['O','N','B','4'],
-for vnlb = ['N'],
+for vnlb = ['B','4'],
 for sigmai = 1:3, sigma = sigmas{sigmai};
-for seqi = [2],
+for t = 1:length(tiles),
 
-	seq = sequences(seqi);
+	seq = tiles(t).seq;
 
 	switch (vnlb)
-	case 'B', u_pat = [nlb3_path seq.name '_s' sigma '_pt4/deno_%03d.png']; % nlb3 pt4
+	case 'B',
+		if seq.channels == 1,
+			u_pat = [ nlb3_mono_path seq.name '_s' sigma '_pt4/deno_%03d.png']; % nlb3 pt4
+		else
+			u_pat = [ nlb3_colo_path seq.name '_s' sigma '_pt4/deno_%03d.png']; % nlb3 pt4
+		end
 	case '4', u_pat = [bm4d_path seq.name '_mp_s' sigma '/deno_%03d.png'];  % bm4d mp
 	case 'O', u_pat = [orig_path seq.name '/%03d.png'];                     % ground truth
-	case 'N', u_pat = [nlb3_path seq.name '_s' sigma '_pt4/nisy_%03d.png']; % noisy
+	case 'N', u_pat = [nisy_path seq.name '_mp_s' sigma '/nisy_%03d.png'];  % noisy
 	end
 
 	u = load_sequence(u_pat, seq.first, seq.last);
@@ -71,12 +141,19 @@ for seqi = [2],
 %	end
 
 	switch (vnlb)
-	case 'B', tile_pat = [nlb3_path seq.name '_s' sigma '_pt4/tile%03d-%03d-%03d_%%03d.png']; % nlb3 pt4
-	case '4', tile_pat = [bm4d_path seq.name '_mp_s' sigma  '/tile%03d-%03d-%03d_%%03d.png']; % bm4d mp
-	case 'O', tile_pat = [orig_path seq.name '/tile%03d-%03d-%03d_%%03d.png'];                     % ground truth
-	case 'N', tile_pat = [nlb3_path seq.name '_s' sigma '_pt4/nisy_tile%03d-%03d-%03d_%%03d.png']; % noisy
+%	case 'B', tile_pat = [nlb3_path seq.name '_s' sigma '_pt4/tile%03d-%03d-%03d_%%03d.png']; % nlb3 pt4
+%	case '4', tile_pat = [bm4d_path seq.name '_mp_s' sigma  '/tile%03d-%03d-%03d_%%03d.png']; % bm4d mp
+%	case 'O', tile_pat = [orig_path seq.name '/tile%03d-%03d-%03d_%%03d.png'];                     % ground truth
+%	case 'N', tile_pat = [nisy_path seq.name '_mp_s' sigma '/nisy_tile%03d-%03d-%03d_%%03d.png']; % noisy
+	case 'B', tile_pat = [seq.name '_s' sigma '_pt4_tile%03d-%03d-%03d_%%03d.png']; % nlb3 pt4
+	case '4', tile_pat = [seq.name '_mp_s' sigma  '_tile%03d-%03d-%03d_%%03d.png']; % bm4d mp
+	case 'O', tile_pat = [seq.name '_tile%03d-%03d-%03d_%%03d.png'];                     % ground truth
+	case 'N', tile_pat = [seq.name '_mp_s' sigma '_nisy_tile%03d-%03d-%03d_%%03d.png']; % noisy
 	end
 
+	tile_i = [tiles(t).tile_b(1) : tiles(t).tile_e(1)];
+	tile_j = [tiles(t).tile_b(2) : tiles(t).tile_e(2)];
+	tile_t = [tiles(t).tile_b(3) : tiles(t).tile_e(3)];
 	save_sequence(u(tile_i, tile_j, :, tile_t), ...
 	              sprintf(tile_pat, tile_i(1), tile_j(1), tile_t(1)));
 
@@ -104,8 +181,6 @@ for seqi = [2],
 %	     ff, psnr_bm4d )
 %	legend('noisy','pt4','pt3','pt2','V-BM4D-mp')
 %	title(seq.name)
-
-	break
 
 end
 end
--- tools/psnr_vs_rank_np_images.m
+++ tools/psnr_vs_rank_np_images.m
@@ -1,7 +1,7 @@
 root_folder =  '/media/pariasm/tera/funes/denoising/projects/video_nlbayes3d/results/vnlbayes/';
 
 stage2_folder = [root_folder 'table_2r_vs_2np_derf/'];
-stage1_folder = [root_folder 'table_1r_vs_1np_bus/'];
+stage1_folder = [root_folder 'table_1r_vs_1np_derf/'];
 
 % % stage 2 ---------------------------------------------------
 % ranks = [10:10:80];
@@ -105,116 +105,164 @@ stage1_folder = [root_folder 'table_1r_vs_1np_bus/'];
 
 
 % stage 1 ---------------------------------------------------
-ranks = [10:10:120];
-nsims = [50:100:650];
-nseqs = 1;
-
-f10 = zeros(length(nsims),length(ranks),nseqs);
-f20 = zeros(length(nsims),length(ranks),nseqs);
-f40 = zeros(length(nsims),length(ranks),nseqs);
-
-f10(:,:,1) = load([stage1_folder 'table_fpsnr_bus_s10']);
-%f10(:,:,2) = load([stage1_folder 'table_fpsnr_football_s10']);
-%f10(:,:,3) = load([stage1_folder 'table_fpsnr_foreman_s10']);
-%f10(:,:,4) = load([stage1_folder 'table_fpsnr_tennis_s10']);
-
-f20(:,:,1) = load([stage1_folder 'table_fpsnr_bus_s20']);
-%f20(:,:,2) = load([stage1_folder 'table_fpsnr_football_s20']);
-%f20(:,:,3) = load([stage1_folder 'table_fpsnr_foreman_s20']);
-%f20(:,:,4) = load([stage1_folder 'table_fpsnr_tennis_s20']);
-
-f40(:,:,1) = load([stage1_folder 'table_fpsnr_bus_s40']);
-%f40(:,:,2) = load([stage1_folder 'table_fpsnr_football_s40']);
-%f40(:,:,3) = load([stage1_folder 'table_fpsnr_foreman_s40']);
-%f40(:,:,4) = load([stage1_folder 'table_fpsnr_tennis_s40']);
-
 ranks = [10:10:100];
-f10 = f10(:,1:end-2,:)
-f20 = f20(:,1:end-2,:)
-f40 = f40(:,1:end-2,:)
+nsims = [50:100:1250];
+seqs = {'bus', 'football', 'foreman', 'tennis'};
 
-mf10 = mean(f10,3);
-mf20 = mean(f20,3);
-mf40 = mean(f40,3);
+for s = 10:10:40,
 
-ef10 = mf10 - max(mf10(:));
-ef20 = mf20 - max(mf20(:));
-ef40 = mf40 - max(mf40(:));
+	ff = zeros(length(nsims),length(ranks),nseqs);
 
-imagesc(ef10,[-1 0]), axis equal,axis tight, colormap gray
-set(gca,'XTick',[1:length(ranks)])
-set(gca,'XTickLabel',ranks)
-set(gca,'YTickLabel',nsims)
-xlabel('rank')
-ylabel('n_{sim}')
-print(gcf, '-depsc2', ['psnr_r1-np1-image_s10_bus']);
+	for i = 1:length(seqs),
+		ff(:,:,i) = load([stage1_folder 'table_fpsnr_' seqs{i} '_s' num2str(s)]);
+	end
 
-imagesc(ef20,[-1 0]), axis equal,axis tight, colormap gray
-set(gca,'XTickLabel',ranks)
-set(gca,'YTickLabel',nsims)
-xlabel('rank')
-ylabel('n_{sim}')
-print(gcf, '-depsc2', ['psnr_r1-np1-image_s20_bus']);
+	for i = 1:length(seqs),
+		m = max(max(ff(:,1:end-1,i)));
+		imagesc(ff(:,:,i), m + [-.5 0]),
+		axis equal,axis tight, colormap default
+		colorbar
+		set(gca,'XTick',[1:length(ranks)])
+		set(gca,'YTick',[1:length(nsims)])
+		set(gca,'XTickLabel',ranks)
+		set(gca,'YTickLabel',nsims)
+		xlabel('r')
+		ylabel('n')
+%		print(gcf, '-depsc2', ['fpsnr_r1-np1-image_s' num2str(s) '_' seqs{i}]);
+	end
 
-imagesc(ef40,[-1 0]), axis equal,axis tight, colormap gray
-set(gca,'XTickLabel',ranks)
-set(gca,'YTickLabel',nsims)
-xlabel('rank')
-ylabel('n_{sim}')
-print(gcf, '-depsc2', ['psnr_r1-np1-image_s40_bus']);
+	mff = mean(ff,3);
+	eff = mff;% - max(mff(:));
+
+	m = max(max(eff(:,1:end-1)));
+	imagesc(eff, m + [-.5 0]),
+	axis equal,axis tight, colormap default
+	colorbar
+	set(gca,'XTick',[1:length(ranks)])
+	set(gca,'YTick',[1:length(nsims)])
+	set(gca,'XTickLabel',ranks)
+	set(gca,'YTickLabel',nsims)
+	xlabel('r')
+	ylabel('n')
+%	print(gcf, '-depsc2', ['fpsnr_r1-np1-image_s' num2str(s) '_average_derf']);
+
+	mff = ff - repmat(max(max(ff(:,1:end-1,:))),[length(nsims) length(ranks) 1]);
+	eff = min(mff,[],3);
+
+	m = max(max(eff(:,1:end-1)));
+	imagesc(eff, m + [-.5 0]),
+	axis equal,axis tight, colormap default
+	colorbar
+	set(gca,'XTick',[1:length(ranks)])
+	set(gca,'YTick',[1:length(nsims)])
+	set(gca,'XTickLabel',ranks)
+	set(gca,'YTickLabel',nsims)
+	xlabel('r')
+	ylabel('n')
+	print(gcf, '-depsc2', ['fpsnr_r1-np1-image_s' num2str(s) '_min_derf']);
+
+end
+
+for s = 10:10:40,
+
+	bb = zeros(length(nsims),length(ranks),nseqs);
+
+	for i = 1:length(seqs),
+		bb(:,:,i) = load([stage1_folder 'table_bpsnr_' seqs{i} '_s' num2str(s)]);
+	end
+
+	for i = 1:length(seqs),
+		m = max(max(bb(:,1:end-1,i)));
+		imagesc(bb(:,:,i), m + [-2 0]),
+		axis equal,axis tight, colormap default
+		colorbar
+		set(gca,'XTick',[1:length(ranks)])
+		set(gca,'YTick',[1:length(nsims)])
+		set(gca,'XTickLabel',ranks)
+		set(gca,'YTickLabel',nsims)
+		xlabel('r')
+		ylabel('n')
+%		print(gcf, '-depsc2', ['bpsnr_r1-np1-image_s' num2str(s) '_' seqs{i}]);
+	end
+
+	mbb = mean(bb,3);
+	ebb = mbb;% - max(mbb(:));
+
+	m = max(max(ebb(:,1:end-1)));
+	imagesc(ebb, m + [-2 0]),
+	axis equal,axis tight, colormap default
+	colorbar
+	set(gca,'XTick',[1:length(ranks)])
+	set(gca,'YTick',[1:length(nsims)])
+	set(gca,'XTickLabel',ranks)
+	set(gca,'YTickLabel',nsims)
+	xlabel('r')
+	ylabel('n')
+%	print(gcf, '-depsc2', ['bpsnr_r1-np1-image_s' num2str(s) '_average_derf']);
+
+end
 
 
-ranks = [10:10:120];
-nsims = [50:100:650];
-nseqs = 1;
-
-f10 = zeros(length(nsims),length(ranks),nseqs);
-f20 = zeros(length(nsims),length(ranks),nseqs);
-f40 = zeros(length(nsims),length(ranks),nseqs);
-
-f10(:,:,1) = load([stage1_folder 'table_time_bus_s10']);
+%ranks = [10:10:100];
+%nsims = [50:100:650];
+%nseqs = 1;
+%
+%f10 = zeros(length(nsims),length(ranks),nseqs);
+%f20 = zeros(length(nsims),length(ranks),nseqs);
+%f30 = zeros(length(nsims),length(ranks),nseqs);
+%f40 = zeros(length(nsims),length(ranks),nseqs);
+%
+%f10(:,:,1) = load([stage1_folder 'table_time_bus_s10']);
 %f10(:,:,2) = load([stage1_folder 'table_time_football_s10']);
 %f10(:,:,3) = load([stage1_folder 'table_time_foreman_s10']);
 %f10(:,:,4) = load([stage1_folder 'table_time_tennis_s10']);
-
-f20(:,:,1) = load([stage1_folder 'table_time_bus_s20']);
+%
+%f20(:,:,1) = load([stage1_folder 'table_time_bus_s20']);
 %f20(:,:,2) = load([stage1_folder 'table_time_football_s20']);
 %f20(:,:,3) = load([stage1_folder 'table_time_foreman_s20']);
 %f20(:,:,4) = load([stage1_folder 'table_time_tennis_s20']);
-
-f40(:,:,1) = load([stage1_folder 'table_time_bus_s40']);
+%
+%f30(:,:,1) = load([stage1_folder 'table_time_bus_s30']);
+%f30(:,:,2) = load([stage1_folder 'table_time_football_s30']);
+%f30(:,:,3) = load([stage1_folder 'table_time_foreman_s30']);
+%f30(:,:,4) = load([stage1_folder 'table_time_tennis_s30']);
+%
+%f40(:,:,1) = load([stage1_folder 'table_time_bus_s40']);
 %f40(:,:,2) = load([stage1_folder 'table_time_football_s40']);
 %f40(:,:,3) = load([stage1_folder 'table_time_foreman_s40']);
 %f40(:,:,4) = load([stage1_folder 'table_time_tennis_s40']);
-
-ranks = [10:10:100];
-f10 = f10(:,1:end-2,:)
-f20 = f20(:,1:end-2,:)
-f40 = f40(:,1:end-2,:)
-
-mf10 = mean(f10,3);
-mf20 = mean(f20,3);
-mf40 = mean(f40,3);
-
-imagesc(mf10,[0 1000]), axis equal,axis tight, colormap gray
-set(gca,'XTickLabel',ranks)
-set(gca,'YTickLabel',nsims)
-xlabel('rank')
-ylabel('n_{sim}')
-print(gcf, '-depsc2', ['time_r1-np1-image_s10_bus']);
-
-imagesc(mf20,[0 1000]), axis equal,axis tight, colormap gray
-set(gca,'XTickLabel',ranks)
-set(gca,'YTickLabel',nsims)
-xlabel('rank')
-ylabel('n_{sim}')
-print(gcf, '-depsc2', ['time_r1-np1-image_s20_bus']);
-
-imagesc(mf40,[0 1000]), axis equal,axis tight, colormap gray
-set(gca,'XTickLabel',ranks)
-set(gca,'YTickLabel',nsims)
-xlabel('rank')
-ylabel('n_{sim}')
-print(gcf, '-depsc2', ['time_r1-np1-image_s40_bus']);
+%
+%mf10 = mean(f10,3);
+%mf20 = mean(f20,3);
+%mf30 = mean(f30,3);
+%mf40 = mean(f40,3);
+%
+%imagesc(mf10,[0 1000]), axis equal,axis tight, colormap gray
+%set(gca,'XTickLabel',ranks)
+%set(gca,'YTickLabel',nsims)
+%xlabel('rank')
+%ylabel('n_{sim}')
+%print(gcf, '-depsc2', ['time_r1-np1-image_s10_average_derf']);
+%
+%imagesc(mf20,[0 1000]), axis equal,axis tight, colormap gray
+%set(gca,'XTickLabel',ranks)
+%set(gca,'YTickLabel',nsims)
+%xlabel('rank')
+%ylabel('n_{sim}')
+%print(gcf, '-depsc2', ['time_r1-np1-image_s20_average_derf']);
+%
+%imagesc(mf30,[0 1000]), axis equal,axis tight, colormap gray
+%set(gca,'XTickLabel',ranks)
+%set(gca,'YTickLabel',nsims)
+%xlabel('rank')
+%ylabel('n_{sim}')
+%print(gcf, '-depsc2', ['time_r1-np1-image_s30_average_derf']);
+%
+%imagesc(mf40,[0 1000]), axis equal,axis tight, colormap gray
+%set(gca,'XTickLabel',ranks)
+%set(gca,'YTickLabel',nsims)
+%xlabel('rank')
+%ylabel('n_{sim}')
+%print(gcf, '-depsc2', ['time_r1-np1-image_s40_average_derf']);
 
 
